b875f4d6270f4721c2e4c7aa57f97c26e58bac21
Smith-Brandon <mormoncook@gmail.com>
2020-04-29 17:50:57 -0700

    ListA and MapA

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,28 @@
 package com.company;
 
+import java.util.*;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        List listA = new ArrayList();
+
+        listA.add("a");
+        listA.add("b");
+        listA.add("c");
+
+        for (Object x : listA) {
+            System.out.println((String)x);
+        }
+
+        Map mapA = new HashMap();
+        mapA.put(1, "A");
+        mapA.put(2, "B");
+        mapA.put(3, "C");
+
+        for (int i = 1; i < 4; i++) {
+            String output = (String)mapA.get(i);
+            System.out.println(output);
+        }
     }
 }
